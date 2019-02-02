8499cfb59611540be0def61f8f94408a5689759e
Sarah <sarahnmurphy93@gmail.com>
2019-02-02 22:48:34 +1100

    1.4 View a main menu of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,15 +1,17 @@
 package com.twu.biblioteca;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
+
+    public static ArrayList<Book> listOfBooks = new ArrayList<Book>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
 
-        ArrayList<Book> listOfBooks = initialiseLibrary();
-
-        printListOfBooks(listOfBooks);
+        listOfBooks = initialiseLibrary();
+        displayMenu();
 
     }
 
@@ -20,11 +22,38 @@ public class BibliotecaApp {
         listOfBooks.add(new Book("Head First Java", "Kathy Sierra & Bert Bates", 2005));
         listOfBooks.add(new Book("Don’t Make Me Think Revisited", "Steve Krug", 2014));
         return listOfBooks;
+    }
 
+    public static ArrayList<String> initialiseMenu() {
+        ArrayList<String> menuOptions = new ArrayList<String>();
+        menuOptions.add("1. View List of Books");
+        return menuOptions;
+    }
+
+    public static void displayMenu(){
+        ArrayList<String> menuOptions = initialiseMenu();
+        String menuChoice;
+
+        System.out.println("Menu");
+        for(String s : menuOptions) {
+            System.out.println(s);
+        }
+
+        System.out.println("Please select a menu item from above. (Enter the item number.)");
+        Scanner userInput = new Scanner(System.in);
+        menuChoice = userInput.next();
+
+        makeMenuChoice(menuChoice);
+    }
+
+    public static void makeMenuChoice(String menuChoice) {
+        if(menuChoice.equals("1")) {
+            printListOfBooks(listOfBooks);
+        }
     }
 
     public static void printListOfBooks(ArrayList<Book> listOfBooks) {
-        System.out.println("Here is a list of books available in the library:");
+        System.out.println("Here is a list of books in the library:");
         for (int i = 0; i<listOfBooks.size(); i++) {
             System.out.println(listOfBooks.get(i).getBook());
         }
