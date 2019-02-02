8e8d451e087b0a20575e4657440ad4339b49a451
Sarah <sarahnmurphy93@gmail.com>
2019-02-02 19:37:01 +1100

    Add Book class

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+
+    public String title;
+    public String author;
+    public int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
